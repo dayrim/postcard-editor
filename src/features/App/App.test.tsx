ca06bde03ba7ba55ca11efef357057966038795b
Dmitrijs Pavlovs <dmitrijs@brandweb.ee>
2021-03-31 19:26:15 +0300

    Initial structure template.

--- src/features/App/App.test.tsx
+++ src/features/App/App.test.tsx
@@ -1,9 +1,9 @@
 import React from 'react';
 import { render, screen } from '@testing-library/react';
-import App from './App';
+import { App } from 'features';
 
 test('renders learn react link', () => {
   render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
+  const linkElement = screen.getByText(/True/i);
   expect(linkElement).toBeInTheDocument();
 });
--- src/index.tsx
+++ src/index.tsx
@@ -1,15 +1,26 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
+import 'assets/global.scss';
+import { App } from 'features';
+import { Provider } from "react-redux";
+import configureStore from "store";
+
 import reportWebVitals from './reportWebVitals';
 
-ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root')
-);
+
+const bootstrap = async () => {
+  const store = configureStore()
+
+  ReactDOM.render(
+    <React.StrictMode>
+      <Provider store={store}>
+        <App />
+      </Provider>,
+    </React.StrictMode>,
+    document.getElementById('root')
+  );
+};
+bootstrap();
 
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
